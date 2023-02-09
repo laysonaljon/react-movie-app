10449dd20fc4d33740dd6394846de07e66a09f5f
laysonaljon <aljonlayson21@gmail.com>
2023-02-09 16:24:50 +0800

    update website

--- src/App.js
+++ src/App.js
@@ -6,13 +6,6 @@ import './App.css';
 import SearchIcon from './search.svg';
 const API_URL = 'https://www.omdbapi.com?apikey=8f675021'
 
-const movie1 = {
-    "Title": "Italian Spiderman",
-    "Year": "2007",
-    "imdbID": "tt2705436",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg"
-  }
 
 const App = () => {
 
